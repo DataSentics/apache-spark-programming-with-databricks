45c872ef7cc6efa7fab802dc88df1204e1ec95ed
ovidiutoma1 <ovidiutoma1>
2022-12-08 14:37:26 +0000

    3.4L Solved

--- Apache-Spark-Programming-with-Databricks/ASP 3 - Functions/ASP 3.4L - Abandoned Carts Lab.py
+++ Apache-Spark-Programming-with-Databricks/ASP 3 - Functions/ASP 3.4L - Abandoned Carts Lab.py
@@ -62,7 +62,9 @@ display(events_df)
 # TODO
 from pyspark.sql.functions import *
 
-converted_users_df = (sales_df.FILL_IN
+converted_users_df = (sales_df
+                      .select('email').dropDuplicates()
+                      .withColumn('converted', lit(True))
                      )
 display(converted_users_df)
 
@@ -98,7 +100,10 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-conversions_df = (users_df.FILL_IN
+conversions_df = (users_df
+                  .join(other = converted_users_df, on = 'email', how = 'outer')
+                  .filter(col('email').isNotNull())
+                  .na.fill(False, subset = 'converted')
                  )
 display(conversions_df)
 
@@ -138,7 +143,9 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-carts_df = (events_df.FILL_IN
+carts_df = (events_df
+            .withColumn('items', explode(col('items')))
+            .groupBy('user_id').agg(collect_set('items.item_id').alias('cart'))
 )
 display(carts_df)
 
@@ -172,7 +179,7 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-email_carts_df = conversions_df.FILL_IN
+email_carts_df = conversions_df.join(other = carts_df, on = 'user_id', how = 'left')
 display(email_carts_df)
 
 # COMMAND ----------
@@ -208,7 +215,8 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-abandoned_carts_df = (email_carts_df.FILL_IN
+abandoned_carts_df = (email_carts_df
+                      .filter((col('converted') == False) & (col('cart').isNotNull()))
 )
 display(abandoned_carts_df)
 
@@ -238,7 +246,10 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-abandoned_items_df = (abandoned_carts_df.FILL_IN
+abandoned_items_df = (abandoned_carts_df
+                      .withColumn('cart', explode('cart'))
+                      .groupBy('cart').count()
+                      .withColumnRenamed('cart', 'items')
                      )
 display(abandoned_items_df)
 
