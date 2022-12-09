3da2e92d2a3db452420cdb7452aaaeb7e6fac8eb
alexandrucheciches <alexandrucheciches>
2022-12-09 15:01:53 +0000

    work_on_lab_5.1cL

--- Apache-Spark-Programming-with-Databricks/ASP 5 - Streaming/ASP 5.1cL - Activity by Traffic Lab.py
+++ Apache-Spark-Programming-with-Databricks/ASP 5 - Streaming/ASP 5.1cL - Activity by Traffic Lab.py
@@ -44,7 +44,12 @@
 # COMMAND ----------
 
 # TODO
-df = FILL_IN
+df = (spark
+      .readStream
+      .option("maxFilesPerTrigger", 1)
+      .format("delta")
+      .load(events_path)
+)
 
 df.isStreaming
 
@@ -69,9 +74,15 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-spark.FILL_IN
+from pyspark.sql.functions import approx_count_distinct, col
 
-traffic_df = df.FILL_IN
+spark.conf.set("spark.sql.shuffle.partitions", sc.defaultParallelism)
+
+traffic_df = (df
+              .groupBy("traffic_source")
+              .agg(approx_count_distinct("user_id").alias("active_users"))
+              .sort(col("traffic_source"))
+             )
 
 # COMMAND ----------
 
@@ -91,6 +102,7 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
+display(traffic_df)
 
 # COMMAND ----------
 
@@ -108,7 +120,13 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-traffic_query = (traffic_df.FILL_IN
+traffic_query = (traffic_df
+                 .writeStream
+                 .queryName("active_users_by_traffic")
+                 .format("memory")
+                 .outputMode("complete")
+                 .trigger(processingTime="1 second")
+                 .start()
 )
 
 # COMMAND ----------
@@ -132,6 +150,7 @@ print("All test pass")
 
 # MAGIC %sql
 # MAGIC -- TODO
+# MAGIC select * from active_users_by_traffic
 
 # COMMAND ----------
 
@@ -156,6 +175,9 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
+for s in spark.streams.active:
+    print(s.name)
+    s.stop()
 
 # COMMAND ----------
 
