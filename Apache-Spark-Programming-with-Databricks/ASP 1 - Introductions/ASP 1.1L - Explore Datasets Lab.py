5ec688cba99ffe16466747e635e8638e95c41fed
LucianFlorin <LucianFlorin>
2022-12-12 06:16:02 +0000

    Lab 5.1c Solutions

--- Apache-Spark-Programming-with-Databricks/ASP 1 - Introductions/ASP 1.1L - Explore Datasets Lab.py
+++ Apache-Spark-Programming-with-Databricks/ASP 1 - Introductions/ASP 1.1L - Explore Datasets Lab.py
@@ -204,6 +204,10 @@ classroom_cleanup()
 
 # COMMAND ----------
 
+spark
+
+# COMMAND ----------
+
 # MAGIC %md-sandbox
 # MAGIC &copy; 2022 Databricks, Inc. All rights reserved.<br/>
 # MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
